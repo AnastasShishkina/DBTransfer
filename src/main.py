c9db1f85411cbdfbddb3ade1cf9630dca85ba09b
user <nasti167@mail.ru>
2025-10-06 10:18:52 +0500

    - fix

--- src/main.py
+++ src/main.py
@@ -1,7 +1,7 @@
 from src.logger.logger import setup_logging
 setup_logging()
 import uvicorn
-from src.fastAPI import app
+from src.fastAPI.app import app
 
 if __name__ == "__main__":
     uvicorn.run(app, host="0.0.0.0", port=8000)
